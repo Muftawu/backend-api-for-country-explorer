990b4a6622fe85fc833c3f77e56c93c6f0649358
Muftawu <moahmmedyiwere@gmail.com>
2023-06-21 11:19:53 +0000

    npass

--- capitals/urls.py
+++ capitals/urls.py
@@ -2,6 +2,6 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('get/', views.get_country, name='get_country'),
+    path('', views.get_country, name='get_country'),
     path('add/', views.add_country, name='add_country'),
 ]
