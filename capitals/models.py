8889854b8b5e4105dd5960214dd127ae079aaca9
Muftawu <moahmmedyiwere@gmail.com>
2023-06-26 11:26:40 +0000

    Added description field to countries

--- capitals/models.py
+++ capitals/models.py
@@ -4,6 +4,10 @@ class countries(models.Model):
     name = models.CharField(max_length=100, null=True, blank=True)
     city = models.CharField(max_length=100, null=True, blank=True)
     image = models.ImageField(upload_to='images', null=True, blank=True)
+    description = models.TextField(null=True, blank=True)
+
+    class Meta:
+        verbose_name_plural = 'Countries'
 
     def __str__(self):
         return self.name 
